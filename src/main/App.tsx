626291e37c1dea2032405e3e8bfa98c162de5d8c
ai73aaa <1qazxcvBG>
2020-03-04 14:47:45 +0300

    init

---
+++ src/main/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            neko-cafes
+        </div>
+    );
+};
+
+export default App;
